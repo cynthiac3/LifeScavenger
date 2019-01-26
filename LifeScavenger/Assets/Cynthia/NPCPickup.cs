62b4e44f0d08ebfb79a9a71919ef6e5fe160d175
Cynthia <cynthia.cherrier@hotmail.com>
2019-01-26 18:59:21 -0500

    added allies stuff

---
+++ LifeScavenger/Assets/Cynthia/NPCPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCPickup : MonoBehaviour
+{
+    public GameObject follower;
+
+    public Transform[] allyHousePositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addFollower(GameObject newFollower) {
+        follower = newFollower;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Door") {
+            follower.GetComponent<NPCFollow>().setDestination(allyHousePositions[0]);
+        }   
+    }
+}
