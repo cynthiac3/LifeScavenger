24bc3158567498fe3a6519f6da102965c9b04d0a
Kevin Raymond-Lamoureux <raymondlamoureux.kevin@gmail.com>
2019-01-27 13:11:23 -0500

    Sound ambiance and death suppose to play

---
+++ LifeScavenger/Assets/SFX/SoundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    AudioSource[] AudioList;
+
+    // Use this for initialization
+    void Start()
+    {
+        AudioList = transform.GetComponents<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void playAmbiance()
+    {
+        AudioList[0].Play();
+    }
+
+    public void playDeathSound()
+    {
+        AudioList[1].Play();
+    }
+
+    /*public void playDie()
+    {
+        AudioList[2].Play();
+    }
+
+    public void playWin()
+    {
+        AudioList[3].Play();
+    }*/
+}
