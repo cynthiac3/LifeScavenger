a54e704cbd4043b8086cae3e1e21a4ae6cc3760a
maeva <fancatsims@hotmail.fr>
2019-01-26 21:03:54 -0500

    Ajouter l'animation d'attaque et l'event quand le zombie donne un coup.

---
+++ LifeScavenger/Assets/Enemies/Scripts/AlertZoneScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemy.ThirdPerson;
+
+public class AlertZoneScript : MonoBehaviour
+{
+    public MonoBehaviour Enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider c)
+    {
+        if(c.tag == "Player")
+        {
+            Debug.Log("OnTriggerEnter()");
+            Enemy.GetComponent<EnemyAICharacterControl>().SetTarget(c);
+        }
+    }
+    void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            Debug.Log("OnTriggerExit()");
+            Enemy.GetComponent<EnemyAICharacterControl>().RemoveTarget();
+        }
+           
+    }
+
+}
---
+++ LifeScavenger/Assets/Enemies/Scripts/EnemyAttack.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class EnemyAttack : MonoBehaviour
+{
+    bool isAttacking = false;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        Debug.Log("OnTriggerEnter Attacking c == " + c.tag != null? c.name.ToString() : "null");
+        if (c.tag == "Player")
+        {
+            isAttacking = true;
+            anim.SetTrigger("Attack");
+            Debug.Log("is Attacking");
+        }
+    }
+    void OnTriggerExit(Collider c)
+    {
+        Debug.Log("OnTriggerExit Attacking");
+        if (c.tag == "Player")
+        {
+            isAttacking = false;
+            anim.SetTrigger("EndAttack");
+            Debug.Log("end Attack");
+        }
+    }
+    void HitEvent()
+    {
+        Debug.Log("BIM!!");
+
+    }
+}
